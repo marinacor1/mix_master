d4be69595b2645821910f2d37c314cce7d9e3749
marinacor1 <marinaspam1234@gmail.com>
2016-04-08 14:41:43 -0600

    adding factory girl

--- spec/features/user_creates_a_song_spec.rb
+++ spec/features/user_creates_a_song_spec.rb
@@ -1,17 +1,18 @@
-# require 'rails_helper'
-#
-# RSpec.feature "user can create a song" do
-#   scenario "they can add a new song to song index" do
-#     artist_name = "Bob Marley"
-#     artist_image_path = "http://cps-static.rovicorp.com/3/JPG_400/MI0003/146/MI0003146038.jpg"
-#
-#     visit artists_path
-#
-#     click_on "Bob Marley"
-#     click_on "New Song"
-#     fill_in "song_title", with: "No Woman, No Cry"
-#     click_on "Create Song"
-#
-#
-#   end
-# end
+require 'rails_helper'
+
+RSpec.feature "user can create a song" do
+  scenario "they can see a page for the song" do
+    artist = create(:artist)
+    song_title = "No Woman, No Cry"
+    artist_image_path = "http://cps-static.rovicorp.com/3/JPG_400/MI0003/146/MI0003146038.jpg"
+
+    visit artist_path(artist)
+
+    click_on "New Song"
+    fill_in "song_title", with: song_title
+    click_on "Create Song"
+
+    expect(page).to have_content song_title
+    expect(page).to have_link artist.name, href: artist_path(artist)
+  end
+end
