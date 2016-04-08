eec5504d64080600e8cbbac57fa313650f69b044
marinacor1 <marinaspam1234@gmail.com>
2016-04-08 16:49:51 -0600

    adding new song link

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   resources :artists do
-    resources :songs, only: [:new]
+    resources :songs, only: [:new, :create]
   end
 end
--- spec/features/user_creates_a_song_spec.rb
+++ spec/features/user_creates_a_song_spec.rb
@@ -7,7 +7,6 @@ RSpec.feature "user can create a song" do
     artist_image_path = "http://cps-static.rovicorp.com/3/JPG_400/MI0003/146/MI0003146038.jpg"
 
     visit artist_path(artist)
-
     click_on "New Song"
     fill_in "song_title", with: song_title
     click_on "Create Song"
