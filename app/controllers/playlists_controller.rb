15168fe43b01b055d4a96ad6fbe0139532694e44
marinacor1 <marinaspam1234@gmail.com>
2016-04-08 23:32:44 -0600

    playlist work;

--- app/controllers/playlists_controller.rb
+++ app/controllers/playlists_controller.rb
@@ -3,6 +3,24 @@ class PlaylistsController < ApplicationController
     @playlist = Playlist.new
   end
 
-  def index
+  def create
+    @playlist = Playlist.create(playlist_params)
+    redirect_to @playlist
   end
+
+  def index
+    @playlists = Playlist.all
+  end
+
+  def show
+    @playlist = Playlist.find(params[:id])
+  end
+
+
+  private
+
+  def playlist_params
+    params.require(:playlist).permit(:name, song_ids: [])
+  end
+
 end
