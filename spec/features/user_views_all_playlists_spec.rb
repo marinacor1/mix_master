8ae7dae78aeba62a79b7c44403d5de63ebbc7502
marinacor1 <marinaspam1234@gmail.com>
2016-04-08 23:37:56 -0600

    adding test for viewing index on playlist

---
+++ spec/features/user_views_all_playlists_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.feature "user views all playlists" do
+  scenario "user can see all playlist on page" do
+    song_one, song_two, song_three = create_list(:song, 3)
+
+    playlist_name = "Mis canciones"
+
+    visit playlist_path
+
+    expect(page).to have_content song_one
+    expect(page).to have_content song_two
+    expect(page).to have_content song_three
+
+    within("li:first") do
+      expect(page).to have_link song_one.title, href: song_path(song_one)
+    end
+
+    within("li:last") do
+      expect(page).to have_link song_three.title, href: song_path(song_three)
+    end
+
+  end
+end
